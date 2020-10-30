5261ccad54da417d00386ec6598d238776643456
Samuel Maskell <samuelmaskell@gmail.com>
2020-10-30 15:51:35 -0700

    Add activities

---
+++ app/src/main/java/com/vandelay/pendingtransition/CrashingActivity.kt
@@ -0,0 +1,15 @@
+package com.vandelay.pendingtransition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class CrashingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crashing)
+        findViewById<Button>(R.id.button_finish).setOnClickListener {
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/vandelay/pendingtransition/MainActivity.kt
+++ app/src/main/java/com/vandelay/pendingtransition/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.vandelay.pendingtransition
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.crash_activity).setOnClickListener {
+            startActivity(Intent(this, CrashingActivity::class.java))
+        }
     }
 }
